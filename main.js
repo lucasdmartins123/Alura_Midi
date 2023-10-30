29c548088f038f0347d07f9e97a44fafb3227374
Lucas Martins <lucasdallmartims@gmail.com>
2023-10-30 16:42:00 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+ function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+    
+    if(elemento && elemento.localName === 'audio'){
+      
+         elemento.play();
+      
+    }
+    else{
+      alert('Elemento nao encontrado ou seletor inválido');
+    }
+ }
+
+ 
+ for(let contador=0; contador<listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function (){
+         tocaSom(idAudio);
+    } 
+
+    tecla.onkeydown = function (evento){
+      if(evento.code === 'Space' || evento.code === 'Enter' ){
+         tecla.classList.add('ativa');
+      }  
+   } 
+
+    tecla.onkeyup = function (){
+      tecla.classList.remove('ativa');
+   } 
+ }
